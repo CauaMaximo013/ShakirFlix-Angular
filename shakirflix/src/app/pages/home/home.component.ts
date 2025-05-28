50a247a703554d9e550946743058946202d61517
CauaMaximo013 <maximocaua13@gmail.com>
2025-05-28 20:28:02 -0300

    cria abas mais populares

--- shakirflix/src/app/pages/home/home.component.ts
+++ shakirflix/src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieApiService } from '../../services/movie-api.service'; 
+import { AutoScrollDirective } from '../../directives/auto-scroll.directive';
+
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, AutoScrollDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -12,9 +14,20 @@ export class HomeComponent {
   constructor (private service: MovieApiService) {}
 
   bannerResults: any = [];
+  trendingMovieResults: any = [];
+  trendingSerieResults: any = [];
+  popularActionMovieResults: any = [];
+  popularComedyMovieResults: any = [];
+  popularFantasyMovieResults: any = [];
+
 
   ngOnInit(): void {
     this.bannerData();
+    this.trendingMovieData();
+    this.trendingSerieData();
+    this.popularActionMovieData();
+    this.popularComedyMovieData();
+    this.popularFantasyMovieData();
   }
 
 
@@ -25,8 +38,37 @@ export class HomeComponent {
       this.bannerResults = result.results;
     }) 
   }
+  //Filmes em destaques
+   trendingMovieData() {
+    this.service.trendingMovieApiData().subscribe((result) =>{
+      this.trendingMovieResults = result.results;
+    })
+   }
 
+   //Séries em Destaques
+   trendingSerieData() {
+    this.service.trendingSerieApiData().subscribe((result) =>{
+      this.trendingSerieResults = result.results;
+    })
+   }
 
-
+   //Filmes Populares
+   popularActionMovieData(){
+    this.service.popularActionMovieApiData().subscribe((result) =>{
+      this.popularActionMovieResults = result.results;
+    })
+   }
+   //Filmes Populares
+   popularComedyMovieData(){
+    this.service.popularActionMovieApiData().subscribe((result) =>{
+      this.popularComedyMovieResults = result.results;
+    })
+   }
+   //Filmes Populares
+   popularFantasyMovieData(){
+    this.service.popularFantasyMovieApiData().subscribe((result) =>{
+      this.popularFantasyMovieResults = result.results;
+    })
+   }
 
 }
--- shakirflix/src/app/services/movie-api.service.ts
+++ shakirflix/src/app/services/movie-api.service.ts
@@ -23,5 +23,32 @@ export class MovieApiService {
     return this.http.get(`${this.baseUrl}/trending/all/weak?language=pt-br`, this.options);
   }
 
-}
+  // Filmes em Destaque do Dia 
+  trendingMovieApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/trending/movie/day?language=pt-br`, this.options);
+  }
+  
+  // Série em Destaque do Dia 
+  trendingSerieApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/trending/tv/day?language=pt-br`, this.options);
+  }
+  
+  // Files de Ação mais Populares 
+  popularActionMovieApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=28&sort_by=popularity.desc`, this.options);
+  }
 
+   // Files de Comedia mais Populares 
+   popularComedyMovieApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=35&sort_by=popularity.desc`, this.options);
+  }
+  
+  // Files de Comedia mais Populares 
+   popularFantasyMovieApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=14&sort_by=popularity.desc`, this.options);
+  }
+
+
+
+
+}
